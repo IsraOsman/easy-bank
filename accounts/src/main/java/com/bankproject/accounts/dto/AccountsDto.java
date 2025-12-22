8a021ea46b7a0fa5de731b0331a975a15434c4d8
isra Osman <israadam1993@gmail.com>
2025-12-22 14:55:31 -0500

    add data validations

--- accounts/src/main/java/com/bankproject/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/bankproject/accounts/dto/AccountsDto.java
@@ -1,11 +1,19 @@
 package com.bankproject.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
+    @NotEmpty(message = "Account number can not be null or empty")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType can not be null or empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress can not be null or empty")
     private String branchAddress;
 }
